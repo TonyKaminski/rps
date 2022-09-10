8d01b4008a2792d9f37a2a09f44467a521380f9c
Tony <ak3983@gmail.com>
2022-09-10 14:55:03 -0400

    all functions

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let choices = ['rock', 'paper', 'scissors'];
 let computerSelection
+let userSelection
 
 function getComputerChoice(){
     computerSelection = choices[Math.floor(Math.random() * choices.length)];
 }
 
-getComputerChoice(choices);
+function getUserChoice(){
+    let rawUserSelection = prompt('Choose Your Weapon')
+    userSelection = rawUserSelection.toLowerCase()
+    if (userSelection == 'scissor') userSelection = 'scissors'
+}
 
-let rawUserSelection = prompt('Choose Your Weapon')
-let userSelection = rawUserSelection.toLowerCase()
-if (userSelection == 'scissor') userSelection = 'scissors'
-
-function evaluate(computerSelection, userSelection){
+function evaluate(){
     if (userSelection == 'rock'){
         if (computerSelection == 'rock'){
             alert('Computer picks rock \n \nLet\'s Call it a Draw')
@@ -43,9 +44,14 @@ function evaluate(computerSelection, userSelection){
     }
 
     else {
-        alert('Invalid weapon \nYou\'re already dead')
+        alert('Invalid weapon \n \nYou\'re already dead')
     }
 }
     
+function playRound(){
+    getComputerChoice()
+    getUserChoice()
+    evaluate()
+}
 
-evaluate(computerSelection, userSelection)
+playRound()
