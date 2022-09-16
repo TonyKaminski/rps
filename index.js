c18b03cf62ea8e25ed9d2ace5976c6573c5968c5
Tony <ak3983@gmail.com>
2022-09-15 21:06:06 -0400

    finalScore()

--- index.js
+++ index.js
@@ -11,34 +11,34 @@ function getComputerChoice(){
 
 function getUserChoice(){
         userSelection = prompt('Choose Your Weapon').toLowerCase()
-    if (userSelection == 'scissor') userSelection = 'scissors'
+    if(userSelection == 'scissor') userSelection = 'scissors'
 }
 
 function evaluate(){
-    if (userSelection == 'rock'){
+    if(userSelection == 'rock'){
         if (computerSelection == 'rock'){
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks rock \n \nLet\'s Call it a Draw')
         }
-        else if (computerSelection == 'paper'){
+        else if(computerSelection == 'paper'){
             ++computerScore
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks paper \n \nLol get rekt')
         }
-        else {
+        else{
             ++userScore
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks scissors \n \nFucking Legend')
         }
     }
     
-    else if (userSelection == 'paper'){
-        if (computerSelection == 'rock'){
+    else if(userSelection == 'paper'){
+        if(computerSelection == 'rock'){
             ++userScore
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks rock \n \nFucking Legend')
         }
-        else if (computerSelection == 'paper'){
+        else if(computerSelection == 'paper'){
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks paper \n \nLet\'s Call it a Draw')
         }
@@ -49,28 +49,35 @@ function evaluate(){
         }
     }
 
-    else if (userSelection == 'scissors'){
-        if (computerSelection == 'rock'){
+    else if(userSelection == 'scissors'){
+        if(computerSelection == 'rock'){
             ++computerScore
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks rock \n \nLol get rekt')
         }
-        else if (computerSelection == 'paper'){
+        else if(computerSelection == 'paper'){
             ++userScore
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks paper \n \nFucking Legend')
         }
-        else {
+        else{
             score = 'You - ' + userScore + '; Evil Robot - ' + computerScore
             alert('Computer picks scissors \n \nLet\'s Call it a Draw')
              }
     }
 
-    else {
-        alert('Invalid weapon \n \nYou\'re already dead')
-    }
+    else alert('Invalid weapon \n \nYou\'re already dead')
     }
 
+function finalScore(){
+    if(userScore > computerScore){
+        alert('You win! ' + userScore + ' to ' + computerScore)
+    }
+    else if(userScore < computerScore){
+        alert('You Lose! ' + userScore + ' to ' + computerScore)
+    }
+    else alert('It\'s a draw!')
+    }
 function playRound(){
     getComputerChoice()
     getUserChoice()
@@ -85,6 +92,7 @@ function playGame(){
         console.log(i)
         playRound()
     }
+    finalScore()
 }
 
 playGame()
